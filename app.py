26b50fbe51f3d376aaa7099129cdece346efa754
andrewkmessios <andrew.messios.writer@gmail.com>
2023-01-03 20:55:58 +0000

    updated app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True)
+server = app.server
 
 navbar = html.Div([
         html.Span('MP Second Jobs', className='navbar-title', style={'margin':'0 50px 0 0','color':'white','fontSize':20}),
